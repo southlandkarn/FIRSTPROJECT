c6d73e0fea54d9fa78d6eb9c269dd13edea5d2a9
southlandkarn <southlandkarn@gmail.com>
2017-09-19 00:16:42 +0300

    commit html

---
+++ src/labs/Main.java
@@ -0,0 +1,12 @@
+package labs;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("  ***     *     *     *");
+        System.out.println("    *   *   *    *   *");
+        System.out.println("    *  *     *    *  *");
+        System.out.println("*   *   ******     **  ");
+        System.out.println(" ***   *     *      * ");
+    }
+}
---
+++ src/labs/SortArr.java
@@ -0,0 +1,19 @@
+package labs;
+
+import java.util.Arrays;
+
+/**
+ * Created by User on 9/14/2017.
+ */
+    public class SortArr{
+        public static void main(String []arg) {
+            int[] numbers = new int[]{1, 23, 3, 8, 2, 7, 4};
+            for(int i = 0; i < numbers.length; i++)
+            //Arrays.sort(numbers);
+
+            for(int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i]);
+            }
+
+        }
+}
